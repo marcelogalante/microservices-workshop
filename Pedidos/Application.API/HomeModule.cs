4a8b1e4c91174ab7b3b79e0890674a0106a16495
Silas Vasconcelos <silasvasconcelos@hotmail.com.br>
2020-06-15 21:01:41 -0300

    adicionando código base

---
+++ Pedidos/Application.API/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.API
+{
+    using Carter;
+    using Microsoft.AspNetCore.Http;
+
+    public class HomeModule : CarterModule
+    {
+        public HomeModule()
+        {
+            Get("/", async (req, res) => All(req, res));
+            Get("/pedidos", async (req, res) => All(req, res));
+        }
+
+        public void All(HttpRequest req, HttpResponse res)
+        {
+            res.WriteAsync("Lista de pedidos");
+        }
+    }
+}
---
+++ Pedidos/Application.API/Program.cs
@@ -0,0 +1,17 @@
+namespace Application.API
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build();
+
+            host.Run();
+        }
+    }
+}
